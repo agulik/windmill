2bab9d7d8487845ad30f762196658e62e3931fbe
Andrew Gulik <andrewgulik@gmail.com>
2020-10-26 13:31:09 -0400

    add mongo to auth service, start auth

---
+++ auth/src/services/password.ts
@@ -0,0 +1,24 @@
+/**
+ * Note on static methods:
+ * Static methods are methods we can access without making an instance of the class.
+ */
+import { scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
+
+const scryptAsync = promisify(scrypt);
+
+export class Password {
+  static async toHash(password: string) {
+    const salt = randomBytes(8).toString('hex');
+    const buff = (await scryptAsync(password, salt, 64)) as Buffer;
+
+    return `${buff.toString('hex')}.${salt}`;
+  }
+
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split('.');
+    const buff = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return buff.toString('hex') === hashedPassword;
+  }
+}
